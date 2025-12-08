0ac968fc2275910d7dff7a5d70c30ad39cb1bf2e
Ionescu Serban-Mihai <serban.ionescu15@gmail.com>
2025-12-08 18:08:37 +0100

    First working version of having a central agency issuing a type of ID. Afterwards, only the citizens can issue ID's for themselves

---
+++ scripts/makeMultipleSoulBornForCitizens.ts
@@ -0,0 +1,104 @@
+import { ethers } from "hardhat";
+
+async function main() {
+
+    // contract address
+    const NATIONAL_ID_ADDR = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+    // Connect to the contract
+    const [user] = await ethers.getSigners();
+    console.log(` Using the user (us): ${user.address}`);
+    const contract = await ethers.getContractAt("MyToken", NATIONAL_ID_ADDR, user);
+
+    // make a list of users to mint for
+    const usersToMintFor = [
+        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+        "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+        "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
+    ];
+
+    // try to mint a new token
+    console.log("------------------------------------------------------\n");
+    console.log("            Testing Mint Function\n");
+    console.log("------------------------------------------------------\n");
+    
+    
+    for (const userAddress of usersToMintFor) {
+        try {
+            console.log("Minting token to address", userAddress);
+            
+            // safeMint is the function
+            const mintTx = await contract.safeMint(userAddress);
+            console.log(`Transaction Sent: ${mintTx.hash}`);
+        
+            // Wait for the block to be mined
+            await mintTx.wait();
+            console.log("Mint Successful!");
+
+            // Check new balance (the number of tokens owned)
+            const balance = await contract.balanceOf(userAddress);
+            console.log(`   New Balance: ${balance.toString()} Token(s)`);
+
+        } catch (error: any) {
+            console.error("Mint Failed for one of the users:", error.shortMessage || error.message);
+        }
+    }
+
+
+    // Test that the token is Soulbound; try to transfer it
+    console.log("------------------------------------------------------\n");
+    console.log("        Testing Transfer (Expect Failure)\n");
+    console.log("------------------------------------------------------\n");
+
+    // We need a random address to try sending it to
+    const randomReceiver = "0x000000000000000000000000000000000000dead"; 
+    
+    for (const userAddress of usersToMintFor) {
+        // const user = await ethers.getSigner(userAddress);
+        console.log(`\n--- Testing transfer for user: ${userAddress} ---`);
+        try {
+            // each contract has a balance (internal function), so find
+            // the balance of our account
+            const balance = await contract.balanceOf(userAddress);
+            
+            if (Number(balance) == 0) {
+                console.log("There are no tokens. Cannot test transfer.");
+                return;
+            }
+            
+            // get one of the tokens of the user (they should have exactly 1)
+            // they shouldn't have multiple, as therough the contract we
+            // permit them to have only 1 Soulbound token. Get the token
+            // and try to transfer it.
+            const tokenId = await contract.tokenOfOwnerByIndex(userAddress, balance - 1n);
+            console.log(`   Attempting to transfer Token ID: ${tokenId.toString()}`);
+            console.log(`   From: ${userAddress}`);
+            console.log(`   To:   ${randomReceiver}`);
+
+            // transfer. Believe this should be SafeTransferFrom; look further into
+            const transferTx = await contract.transferFrom(userAddress, randomReceiver, tokenId);
+            
+            console.log("Waiting for confirmation");
+            await transferTx.wait();
+            
+            // print a message if it didn't fail; that means that it got transferred
+            console.log("BAD: Transfer SUCCEEDED! (Soulbound token got transferred!)");
+
+        } catch (error: any) {
+            // there are some errors that we expect, others not. We expect a soulbound transfer to fail
+            // and revert the transaction.
+            if (error.message.includes("Soulbound: Transfer failed") || error.message.includes("reverted")) {
+                console.log("GOOD: Transfer failed as expected!");
+                console.log(`   Reason: "${error.shortMessage || 'Transaction Reverted'}"`);
+            } else {
+                console.log("BAD: Transfer failed, but for an unexpected reason:");
+                console.log(error);
+            }
+        }
+    }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
