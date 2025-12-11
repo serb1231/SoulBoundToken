22948041418f830fa489ccc2a761d9b662e5f61b
Ionescu Serban-Mihai <serban.ionescu15@gmail.com>
2025-12-11 12:45:07 +0100

    forced a robot to make me a readme, as writting in good english is not the reason I became programmer. Also, modified the testing file to reflect the new name of the nationalID contract file

--- test/test.ts
+++ test/test.ts
@@ -1,15 +1,15 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("MyToken", function () {
+describe("NationalID", function () {
   it("Test contract", async function () {
-    const ContractFactory = await ethers.getContractFactory("MyToken");
+    const ContractFactory = await ethers.getContractFactory("NationalID");
 
     const initialOwner = (await ethers.getSigners())[0].address;
 
     const instance = await ContractFactory.deploy(initialOwner);
     await instance.waitForDeployment();
 
-    expect(await instance.name()).to.equal("MyToken");
+    expect(await instance.name()).to.equal("NationalID");
   });
 });
